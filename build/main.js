5eb18b200fe501742e70e0f18512532db46d8e0b
osamakh <osamakh.tj@gmail.com>
2023-11-06 21:07:27 +0200

    update the project structure

--- build/main.js
+++ build/main.js
@@ -1,52 +1,98 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const palindrome_1 = require("./palindrome");
 const Nationalize_1 = require("./Nationalize");
 const Lower_1 = require("./Lower");
 const Digits_1 = require("./Digits");
 const Armstrong_1 = require("./Armstrong");
+const readlineSync = __importStar(require("readline-sync"));
 class List {
     constructor() {
         this.key = 0;
+        this.Exit_flag = true;
     }
     print_result(val) {
         console.log("the result is: " + val);
     }
-    run(key) {
-        switch (key) {
-            case 1:
-                this.operation = new palindrome_1.Palindrome();
-                this.print_result(this.operation.command_param_number(132));
-                break;
-            case 2:
-                this.operation = new Lower_1.Lower();
-                this.print_result(this.operation.command_param_string("HeLlo"));
-                break;
-            case 3:
-                this.operation = new Digits_1.Digits();
-                this.print_result(this.operation.command_param_string("123"));
-                break;
-            case 4:
-                this.operation = new Armstrong_1.Armstrong();
-                this.print_result(this.operation.command_param_number(1634));
-                break;
-            case 5:
-                this.operation = new Nationalize_1.Nationalize();
-                this.print_result(this.operation.command_param_string("Yuval"));
-                break;
-            case 6:
-                break;
-            default:
-                break;
-        }
+    print_instruction_list() {
+        console.log("The available commands are:");
+        console.log("1 — Palindrome — Check if the input is a palindrome");
+        console.log("2 - Lower - Check if all characters in the input are lowercase");
+        console.log("3 - Digits - Check if all characters in the input are digits");
+        console.log("4 - Armstrong - Check if the input is an 'Armstrong Number'");
+        console.log("5 - Nationalize - Check the nationality probability of a given first name        ");
+        console.log("6 - Exit - Exit successfully from the application");
+        // Enter the number of the command: 4
+        // Enter the input: 371
+        // The answer is: True
+    }
+    print_input() {
+        let param = readlineSync.question('Enter the input: ');
+        return param;
+    }
+    run() {
+        do {
+            this.print_instruction_list();
+            let key = parseInt(readlineSync.question('Enter the number of the command: '));
+            switch (key) {
+                case 1:
+                    this.operation = new palindrome_1.Palindrome();
+                    this.print_result(this.operation.command_param_number(parseInt(this.print_input())));
+                    break;
+                case 2:
+                    this.operation = new Lower_1.Lower();
+                    this.print_result(this.operation.command_param_string(this.print_input()));
+                    break;
+                case 3:
+                    this.operation = new Digits_1.Digits();
+                    this.print_result(this.operation.command_param_string(this.print_input()));
+                    break;
+                case 4:
+                    this.operation = new Armstrong_1.Armstrong();
+                    this.print_result(this.operation.command_param_number(parseInt(this.print_input())));
+                    break;
+                case 5:
+                    this.operation = new Nationalize_1.Nationalize();
+                    this.print_result(this.operation.command_param_string(this.print_input()));
+                    break;
+                case 6:
+                    this.Exit_flag = false;
+                    break;
+                default:
+                    break;
+            }
+        } while (this.Exit_flag);
     }
 }
 var list = new List();
-list.run(1);
-list.run(2);
-list.run(3);
-list.run(4);
-list.run(5);
+list.run();
+// list.run(2)
+// list.run(3)
+// list.run(4)
+// list.run(5)
 // list.run(1)
 // console.log(list.IsPalindrome(5421))
 // console.log(list.IsLower("HeLlo"))
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { Nationalize } from "./Nationalize"
 import {Lower} from "./Lower"
 import {Digits} from "./Digits"
 import {Armstrong} from "./Armstrong"
+import * as readlineSync from 'readline-sync';
 
 class List{
 private key:number = 0;
 private operation:any;
+private Exit_flag:boolean=true;
     public constructor(){
 
     }
@@ -17,54 +19,77 @@ private operation:any;
 
 
     private print_instruction_list():void{
+        console.log("The available commands are:")
+        console.log("1 — Palindrome — Check if the input is a palindrome")
+        console.log("2 - Lower - Check if all characters in the input are lowercase")
+        console.log("3 - Digits - Check if all characters in the input are digits")
+        console.log("4 - Armstrong - Check if the input is an 'Armstrong Number'")
+        console.log("5 - Nationalize - Check the nationality probability of a given first name        ")
+        console.log("6 - Exit - Exit successfully from the application\n")
+
+// Enter the number of the command: 4
+// Enter the input: 371
+// The answer is: True
 
     }
-    public run(key:number):void{
 
 
+    private print_input():string{
+        let param: string = readlineSync.question('Enter the input: ');
+        return param;
+    }
+
+    public run():void{
+do{
+    this.print_instruction_list();
+
+    let key: number = parseInt(readlineSync.question('Enter the number of the command: '));
        switch (key) {
     case 1:
          this.operation =new Palindrome()
-         this.print_result(this.operation.command_param_number(132))
+         this.print_result(this.operation.command_param_number(parseInt(this.print_input())))
 
         break;
 
     case 2:
         this.operation = new Lower()
-        this.print_result(this.operation.command_param_string("HeLlo"))
+        this.print_result(this.operation.command_param_string(this.print_input()))
 
         break;
     case 3:
         this.operation = new Digits()
-        this.print_result(this.operation.command_param_string("123"))
+        this.print_result(this.operation.command_param_string(this.print_input()))
 
         break;
     case 4:
+        
         this.operation = new Armstrong()
-        this.print_result(this.operation.command_param_number(1634))
+        this.print_result(this.operation.command_param_number(parseInt(this.print_input())))
 
         break;
     case 5:
         this.operation = new Nationalize()
-        this.print_result(this.operation.command_param_string("Yuval"))
+        this.print_result(this.operation.command_param_string(this.print_input()))
 
         break;
     case 6:
-
+        this.Exit_flag=false
         break;
     default:
         break;
 } 
+}while(this.Exit_flag)
+   
     }
 
 }
 
 var list = new List();
-list.run(1)
-list.run(2)
-list.run(3)
-list.run(4)
-list.run(5)
+list.run()
+// list.run(2)
+// list.run(3)
+// list.run(4)
+// list.run(5)
 // list.run(1)
 // console.log(list.IsPalindrome(5421))
 // console.log(list.IsLower("HeLlo"))
